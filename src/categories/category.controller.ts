c7fbd7001adea4047c087a0461b9f6e48c2a097e
Mohamed Abobaker <mohamed.abobaker@ANSGRP-002903.local>
2019-10-24 14:46:22 +0100

    added delete endpoint to category

--- src/categories/category.controller.ts
+++ src/categories/category.controller.ts
@@ -1,18 +1,65 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Req, Res, Param, Delete } from '@nestjs/common';
 import { CategoryService } from './category.service'
 import { CategoryEntity } from 'src/entities/category.entity';
+import { Request, Response } from 'express';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService, ) { }
 
+  @Delete(':id')
+  async deleteCategory(
+    @Req() request: Request,
+    @Res() response: Response,
+    @Param('id') id: 'uuid'
+  ): Promise<any> {
+    const deleted = await this.categoryService.deleteCategory(id);
+    try {
+      response
+        .status(200)
+        .json({
+          message: deleted
+        })
+    }
+    catch{ }
+  }
+
   @Post()
-  async addCategory(@Body() payload: any): Promise<any> {
+  async addCategory(
+    @Body() payload: any
+  ): Promise<CategoryEntity> {
     return await this.categoryService.addCategory(payload)
   }
 
   @Get()
-  async getCategories(): Promise<CategoryEntity[]> {
-    return await this.categoryService.getCategories()
+  async getCategories(
+    @Req() request: Request,
+    @Res() response: Response,
+    @Param('id') id: string
+  ): Promise<Response> {
+
+    try {
+
+      console.log(request.params);
+
+      const categories = await this.categoryService.getCategories()
+      return response
+        .status(200)
+        .json({
+          data: categories,
+          error: null
+        });
+    }
+    catch (error) {
+      return response
+        .status(501)
+        .json({
+          data: null,
+          error: {
+            message: error.message,
+            code: error.code
+          }
+        });
+    }
   }
 }
--- src/categories/category.service.ts
+++ src/categories/category.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm';
 import { CategoryEntity } from '../entities/category.entity'
-import { Repository } from 'typeorm';
+import { Repository, InsertResult } from 'typeorm';
 
 @Injectable()
 export class CategoryService {
@@ -10,12 +10,32 @@ export class CategoryService {
     readonly categoryEntity: Repository<CategoryEntity>
   ) { }
 
+
   async getCategories(): Promise<CategoryEntity[]> {
+    // throw new Error('No database connection');
+
     const results = await this.categoryEntity.find();
     return results;
   }
 
-  async addCategory(payload): Promise<any> {
-    await this.categoryEntity.createQueryBuilder().insert().values(payload).returning('*').execute()
+
+  async addCategory(payload: any): Promise<CategoryEntity> {
+
+    const response: InsertResult = await this.categoryEntity
+      .createQueryBuilder().insert().values(payload).returning('*').execute();
+
+    const createdCat: CategoryEntity = response.raw[0];
+    return createdCat;
+  }
+
+  async deleteCategory(id: any): Promise<any> {
+    const where = 'id = :id';
+    const query = {
+      id,
+    };
+    const response = await this.categoryEntity
+      .createQueryBuilder().delete().where(where, query).execute()
+
+    return response.raw
   }
 }
