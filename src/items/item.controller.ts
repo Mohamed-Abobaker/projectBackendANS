fc3a3afd1e5b89cefc3585c0c1c7e87b526f794f
Mohamed Abobaker <mohamed.abobaker@ANSGRP-002903.local>
2019-10-29 07:56:14 +0000

    added a post route for items

--- src/items/item.controller.ts
+++ src/items/item.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Post, Body } from '@nestjs/common'
 import { ItemService } from './item.service'
+import { ItemEntity } from 'src/entities/item.entity'
 
 @Controller('items')
 export class ItemController {
   constructor(private readonly itemService: ItemService) { }
 
   @Get()
-  async getAllItems() {
+  async getAllItems(): Promise<ItemEntity[]> {
     const items = await this.itemService.getItems()
     return items
+  }
+
+  @Post()
+  async postNewItem(@Body() payload: any): Promise<ItemEntity> {
+    const response = await this.itemService.postItem(payload)
+    return response
   }
 }
--- src/items/item.service.ts
+++ src/items/item.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { ItemEntity } from '../entities/item.entity'
-import { Repository } from 'typeorm'
+import { Repository, InsertResult } from 'typeorm'
 
 @Injectable()
 export class ItemService {
@@ -13,5 +13,13 @@ export class ItemService {
   async getItems(): Promise<ItemEntity[]> {
     const items = await this.itemEntityRepo.find();
     return items
+  }
+
+  async postItem(payload: any): Promise<ItemEntity> {
+    const response: InsertResult = await this.itemEntityRepo
+      .createQueryBuilder().insert().values(payload).returning('*').execute()
+
+    const createdItem: ItemEntity = response.raw[0];
+    return createdItem
   }
 }
