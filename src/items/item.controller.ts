70d498d2e19513a9ada525ddd6ab02114e7805ac
Mohamed Abobaker <mohamed.abobaker@ANSGRP-002903.local>
2019-10-29 08:47:37 +0000

    added delete path for items

--- src/items/item.controller.ts
+++ src/items/item.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Post, Body } from '@nestjs/common'
+import { Controller, Get, Post, Body, Delete, Param, Res } from '@nestjs/common'
 import { ItemService } from './item.service'
 import { ItemEntity } from 'src/entities/item.entity'
+import { Request, Response } from 'express';
+
 
 @Controller('items')
 export class ItemController {
@@ -12,9 +14,19 @@ export class ItemController {
     return items
   }
 
+
   @Post()
   async postNewItem(@Body() payload: any): Promise<ItemEntity> {
     const response = await this.itemService.postItem(payload)
     return response
+  }
+
+  @Delete(':id')
+  async deleteAnItem(
+    @Res() response: Response,
+    @Param('id') id: string
+  ): Promise<any> {
+    const deleted = await this.itemService.deleteItem(id)
+    return response.status(deleted.statusCode).json(deleted.data)
   }
 }
--- src/items/item.service.ts
+++ src/items/item.service.ts
@@ -15,6 +15,7 @@ export class ItemService {
     return items
   }
 
+
   async postItem(payload: any): Promise<ItemEntity> {
     const response: InsertResult = await this.itemEntityRepo
       .createQueryBuilder().insert().values(payload).returning('*').execute()
@@ -22,4 +23,44 @@ export class ItemService {
     const createdItem: ItemEntity = response.raw[0];
     return createdItem
   }
+
+
+  async deleteItem(id: string): Promise<any> {
+    try {
+      const item = await this.itemEntityRepo
+        .createQueryBuilder().select('*')
+        .where('"id"=:id', { id }).execute();
+
+      if (item[0] == null) {
+        return {
+          statusCode: 404,
+          data: {
+            message: `No item found with ID ${id}`
+          }
+        }
+      }
+
+      const response = await this.itemEntityRepo
+        .createQueryBuilder().delete()
+        .where('"id"=:id', { id }).execute();
+
+      return {
+        statusCode: 200,
+        data: {
+          message: `Item ID ${id} successfully deleted`,
+          other: response.raw[0]
+        }
+      }
+    }
+    catch (error) {
+      return {
+        statusCode: 500,
+        data: {
+          message: error.message
+        }
+      }
+    }
+  }
+
+
 }
