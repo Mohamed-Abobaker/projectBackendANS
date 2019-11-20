42ff2888df77b05e9e8e40918f71dc36a0b5178b
Mohamed Abobaker <mohamed.abobaker@ANSGRP-002903.local>
2019-11-20 11:12:59 +0000

    config file update

--- ormconfig.js
+++ ormconfig.js
@@ -2,19 +2,29 @@ const { hostName, dataBaseName, password, userName } = require('./userDetails')
 
 // const inProd = process.env.NODE_ENV
 const inProd = true
-const host = inProd ? 'ec2-54-243-44-102.compute-1.amazonaws.com' : hostName;
-const database = inProd ? 'devhemnpqs7jqr' : dataBaseName;
-const user = inProd ? 'atovecnpivswjq' : userName;
-const passwordNow = inProd ? '51a46280f1e23d8e82ca8b03db59333cf604df69014275aa1207378f3d10d7a2' : password;
+console.log(process.env)
+
+// port: 5432,
+// host: inProd ? 'ec2-54-243-44-102.compute-1.amazonaws.com' : hostName;
+// database: inProd ? 'devhemnpqs7jqr' : dataBaseName;
+// username: inProd ? 'atovecnpivswjq' : userName;
+// password: inProd ? '51a46280f1e23d8e82ca8b03db59333cf604df69014275aa1207378f3d10d7a2' : password;
+// }
+
+const dbCreds = inProd
+  ? { url: process.env.DATABASE_URL }
+  : {
+    port: 5432,
+    host: hostName,
+    database: dataBaseName,
+    username: userName,
+    password
+  }
 
 
 module.exports = {
   "type": "postgres",
-  "host": host,
-  "port": 5432,
-  "username": user,
-  "password": passwordNow,
-  "database": database,
+  ...dbCreds,
   "entities": [
     "dist/**/*.entity{.ts,.js}"
   ],
