24eaafb917ff7f570f67d34d1d7cb581fd68d1dc
Mohamed Abobaker <mohamed.abobaker@ANSGRP-002903.local>
2019-11-20 08:34:57 +0000

    moved senesitive info into private file

---
+++ ormconfig.js
@@ -0,0 +1,25 @@
+const { hostName, dataBaseName, password, userName } = require('./userDetails')
+
+const inProd = process.env.NODE_ENV
+const host = inProd ? 'ec2-54-243-44-102.compute-1.amazonaws.com' : hostName;
+const database = inProd ? 'devhemnpqs7jqr' : dataBaseName;
+const user = inProd ? 'atovecnpivswjq' : userName;
+const passwordNow = inProd ? '51a46280f1e23d8e82ca8b03db59333cf604df69014275aa1207378f3d10d7a2' : password;
+
+
+module.exports = {
+  "type": "postgres",
+  "host": host,
+  "port": 5432,
+  "username": user,
+  "password": passwordNow,
+  "database": database,
+  "entities": [
+    "dist/**/*.entity{.ts,.js}"
+  ],
+  "synchronize": true,
+  "migrations": ["dist/typeorm/migration/**/*.js"],
+  "cli": {
+    "migrationsDir": "typeorm/migration"
+  }
+}
